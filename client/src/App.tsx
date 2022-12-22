2f49f7d6430223cbce6a9659b9a9c38712b41499
barooahn <barooahn@gmail.com>
2022-12-22 10:46:15 +0000

    Added frontend and form

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./App.css";
+import DataInput from "./components/DataInput";
+
+function App() {
+	return (
+		<div className='App'>
+			<DataInput />
+		</div>
+	);
+}
+
+export default App;
