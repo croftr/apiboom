e2f04575c7a3a138d40679034ec8d46ff08dd96b
robin croft <rob@robincroft.com>
2022-12-27 17:25:29 +0000

    add data detection

--- client/src/components/RegisterPage.tsx
+++ client/src/components/RegisterPage.tsx
@@ -31,27 +31,90 @@ const RegisterPage: React.FC<RegisterPageProps> = (props) => {
 	const [serviceName, setServiceName] = useState("");
 	const [dataType, setDataType] = useState("text");
 	const [data, setData] = useState("");
+	const [dataDesc, setDataDesc] = useState("");
+	const [dataId, setDataId] = useState("");
+	const [dataDetectionText, setDataDetectionText] = useState("");
+
+	const [isFormValid, setIsFormValid] = useState(false);
+
+	const onDetectDataFormat = (str: string) => {				
+		if (/^\s*(\{[^\{\}]*\}|\[[^\[\]]*\])\s*$/.test(str)) {			
+			setDataDetectionText('json');		
+		} else if (/^\s*</.test(str)) {			
+			setDataDetectionText('xml');
+		} else if (/[,\n]/.test(str)) {			
+			
+			const rows:Array<String> = str.split('\n');
+
+			const columnCount:number = rows[0].split(',').length;
+			const rowCount:number = rows.length;
+
+			setDataDetectionText(`csv: ${columnCount} columns ${rowCount} rows`);
+
+		} else {			
+			setDataDetectionText('plain text');
+		}
+	}
+
+	const validateForm = (type = dataType) => {
+		if (serviceName && data && dataDesc && dataId && type === 'text') {
+			setIsFormValid(true);
+		} else {
+			setIsFormValid(false);
+		}
+	}
 
 	const onChangeServiceName = (event: React.ChangeEvent<HTMLInputElement>) => {
 		setServiceName(event.target.value);
+		validateForm();
+	};
+
+	const onChangeDataDesc = (event: React.ChangeEvent<HTMLInputElement>) => {
+
+		const value: string = event.target.value
+
+		setDataDesc(value);
+
+		setDataId(value.split(' ')[0]);
+
+		validateForm();
+	};
+
+	const onChangeDataId = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setDataId(event.target.value);
+		validateForm();
 	};
 
 	const handleChangeDataType = (event: SelectChangeEvent) => {
-		setDataType(event.target.value as string);
+
+		const type: string = event.target.value as string
+		validateForm(type);
+		setDataType(type);
+
 	};
 
 
 	const onChangeData = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-		setData(event.target.value);
+
+		const data: string = event.target.value;
+
+		setData(data);
+
+		if (data) {
+			onDetectDataFormat(event.target.value);
+		}
+
+		validateForm();
 	}
 
 	const handleSubmit = () => {
-		// event.preventDefault();
 
 		const payload = {
 			serviceName,
 			dataType,
-			data
+			data,
+			dataDesc,
+			dataId
 		}
 
 		// Send the data to the Node.js application using axios
@@ -76,7 +139,7 @@ const RegisterPage: React.FC<RegisterPageProps> = (props) => {
 			/>
 
 			<FormControl style={{ border: '0px solid', width: '70vw' }} sx={{ m: 3 }} component="fieldset" variant="standard">
-				
+
 				<TextField
 					value={serviceName}
 					onChange={onChangeServiceName}
@@ -87,9 +150,10 @@ const RegisterPage: React.FC<RegisterPageProps> = (props) => {
 
 				<Select
 					style={{ marginTop: 12 }}
+					variant='outlined'
 					labelId="demo-simple-select-label"
 					id="demo-simple-select"
-					value={dataType}					
+					value={dataType}
 					onChange={handleChangeDataType}
 				>
 					<MenuItem value='text'>text</MenuItem>
@@ -99,26 +163,60 @@ const RegisterPage: React.FC<RegisterPageProps> = (props) => {
 				</Select>
 				<FormHelperText style={{ marginBottom: 12 }}>What format is your data in?</FormHelperText>
 
+				<div style={{ display: 'flex' }}>
+
+					<div style={{ flex: 3 }}>
+						<TextField
+							fullWidth
+							value={dataDesc}
+							label="Data description"
+							placeholder='e.g cars I fixed or customer bookings'
+							onChange={onChangeDataDesc}
+							id="outlined-basic"
+							variant="outlined"
+						/>
+						<FormHelperText style={{ marginBottom: 12 }}>Data description</FormHelperText>
+					</div>
+
+					<TextField
+						style={{ flex: 1 }}
+						value={dataId}
+						onChange={onChangeDataId}
+						id="outlined-basic"
+						label="Data Id"
+						variant="outlined"
+					/>
+				</div>
+
+
 				{dataType === 'text' && (
 					<TextareaAutosize
 						value={data}
 						onChange={onChangeData}
 						aria-label="minimum height"
 						minRows={10}
-						placeholder="Your data in CSV format"
-						style={{							
+						placeholder="Your data as plain text"
+						style={{
 							border: '1px solid lightgrey'
 						}}
 					/>
 				)}
 
+				<div id='dataDetection'>
+					<Typography>{dataDetectionText}</Typography>
+				</div>
+
 				{dataType !== 'text' && (
-					<Typography>{`${dataType} data not yet supported`}</Typography>
+					<div style={{ padding: 8 }}>
+						<Typography color='error'>{`${dataType} data not yet supported`}</Typography>
+					</div>
+
 				)}
 
-				<Button 					
+				<Button
+					disabled={!isFormValid}
 					onClick={handleSubmit}
-					style={{ marginTop: 12 }} 
+					style={{ marginTop: 12 }}
 					variant="contained">Submit
 				</Button>
 
